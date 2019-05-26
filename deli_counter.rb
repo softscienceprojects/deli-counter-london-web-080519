2c718853272ac5f27f4cd5b5fe4e2fd8a18b271a
erin johnson <erin.nicole.johnson@gmail.com>
2019-05-26 14:01:37 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,20 @@
 # Write your code here.
+
+ def line(katz_deli)
+   if katz_deli.empty? == true
+     puts "The line is currently empty."
+   else
+     # greet = "The line is currently: "
+     katz_deli.each_with_index  { |name, index|
+     puts "The line is currently: #{index + 1}. #{name} "
+   }
+   end
+ end
+
+ # def take_a_number(num)
+ #
+ # end
+ #
+ # def now_serving
+ #
+ # end
