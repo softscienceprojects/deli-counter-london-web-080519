8becda150f17f5260ad614fe7c18272824d3600d
erin johnson <erin.nicole.johnson@gmail.com>
2019-05-26 14:18:01 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,9 +5,9 @@
      puts "The line is currently empty."
    else
      # greet = "The line is currently: "
-     katz_deli.each_with_index do |name, index|
+     katz_deli.each_with_index { |name, index|
      puts "The line is currently: #{index + 1}. #{name} "
-      end
+    }
    end
  end
 
