b1eeaeb86cc86848ebc46bbe5a29763f288c236d
erin johnson <erin.nicole.johnson@gmail.com>
2019-05-26 14:53:01 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -21,6 +21,6 @@
    if katz_deli.empty? == true
      puts "There is nobody waiting to be served!"
    else
-     puts "Currently serving #{katz_deli}"
+     puts "Currently serving #{katz_deli}."
    end
  end
